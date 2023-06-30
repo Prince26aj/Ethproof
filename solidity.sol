817ecc7d23a2957aacd6745497f51c3730d81720
Prince26aj <137023364+Prince26aj@users.noreply.github.com>
2023-06-30 16:50:31 +0530

    Update solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -19,8 +19,8 @@ pragma solidity 0.8.18;
 contract MyToken {
 
     // public variables here
-    string public tokenName = "MXP";
-    string public abbrv = "MX";
+    string public tokenName = "MPR";
+    string public abbrv = "MR";
     uint public totalSupply = 0;
     
 
